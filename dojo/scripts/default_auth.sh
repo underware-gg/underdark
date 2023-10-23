88702dd9eed2a501521943d200bb688ff1aae85d
R13 <rbentos@yahoo.com>
2023-10-23 13:45:18 -0300

    slot env config

--- dojo/scripts/default_auth.sh
+++ dojo/scripts/default_auth.sh
@@ -5,11 +5,14 @@ pushd $(dirname "$0")/..
 echo "Authorizing models..."
 
 export WORLD_ADDRESS="0x6150655e17345af3d45d23f9b1d04a4c5e485dfed5bf55cbd6f53063aaa42a";
+export CONTRACT_ADDRESS="0x1ecc984f2f872bdadd43ab1762c87de3d445b8eb134e6b475f37f23e8a551a3";
 
 # enable system -> model authorizations
 MODELS=("Chamber" "Map" "State" "Tile" "Score" )
 for model in ${MODELS[@]}; do
-    sozo auth writer --world $WORLD_ADDRESS $model 0x1ecc984f2f872bdadd43ab1762c87de3d445b8eb134e6b475f37f23e8a551a3
+    sozo auth writer --world $WORLD_ADDRESS $model $CONTRACT_ADDRESS
+    # SLOT
+    # sozo auth writer --world $WORLD_ADDRESS $model $CONTRACT_ADDRESS --rpc-url https://api.cartridge.gg/x/underdark/katana --account-address 0x1ea3a2c42fa9e202e503d44323506745def44b87ad78742a8ce009b31c9ee84
 done
 
 
