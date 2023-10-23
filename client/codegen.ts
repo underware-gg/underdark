35d49523335f109015f50df9793dbed3c05f3937
R13 <rbentos@yahoo.com>
2023-10-23 00:44:23 -0300

    Slot migration config

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,7 +3,9 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://127.0.0.1:8080/graphql",
+  // schema: "http://127.0.0.1:8080/graphql",
+  // SLOT
+  schema: "https://api.cartridge.gg/x/underdark/torii/graphql",
   documents: "src/**/*.graphql",
   generates: {
     'src/generated/graphql.ts': {
