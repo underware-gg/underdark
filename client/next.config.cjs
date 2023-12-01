cf9a50a2633fe613141812a98a7d6086227a0364
R13 <rbentos@yahoo.com>
2023-12-01 00:20:18 -0300

    Fixed ethers Module not found warnings
    https://github.com/ethers-io/ethers.js/issues/4428

--- client/next.config.cjs
+++ client/next.config.cjs
@@ -7,15 +7,12 @@ const nextConfig = {
   transpilePackages: [
     'react-ascii-text',
   ],
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.externals.push({
-        bufferutil: "bufferutil",
-        "utf-8-validate": "utf-8-validate",
-        "supports-color": "supports-color"
-      });
-    }
-    return config;
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    })
+    return config
   },
   async redirects() {
     return [
